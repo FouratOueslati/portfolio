6dda9e9b14eeff5eb4b135c5e8106fb5563a151f
FouratOueslati <fourat.oueslati@gmx.de>
2025-04-10 16:45:38 +0200

    checkbox and send

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
@@ -15,18 +16,51 @@ export class ContactComponent {
     email: '',
     message: ''
   };
+  isChecked = false;
+  showError = false;
+  imageSrc: string = './assets/Ellipse 32.png';
+
+  
+  toggleCheckbox() {
+    this.isChecked = !this.isChecked;
+    if (this.isChecked) {
+      this.showError = false;
+    }
+  }
+
+
+  changeImage(isHover: boolean) {
+    if (this.showError) {
+      this.imageSrc = './assets/Ellipse red.png'; 
+    } else if (isHover && !this.isChecked) {
+      this.imageSrc = './assets/hover checkbox.png';
+    } else {
+      this.imageSrc = this.isChecked ? './assets/checked.png' : './assets/Ellipse 32.png'; 
+    }
+  }
+
 
   sendEmail() {
-    emailjs.send(
-      'service_yva67mf',
-      'template_9d0negb',
-      this.formData,
-      'Qnkf6zGcxoZPBJ5Ij'
-    )
-    .then(() => {
-      alert('Email sent successfully!');
-    }, (error) => {
-      alert('Failed to send email: ' + error.text);
-    });
+  
+    if (!this.isChecked) {
+      this.showError = true; 
+      return;
+    }
+    if (this.formData.name && this.formData.email && this.formData.message) {
+      emailjs.send(
+        'service_yva67mf',  // Your EmailJS service ID
+        'template_9d0negb',  // Your EmailJS template ID
+        this.formData,       // Form data
+        'Qnkf6zGcxoZPBJ5Ij'  // Your EmailJS user/public key
+      )
+      .then(() => {
+        alert('Email sent successfully!');
+      })
+      .catch((error) => {
+        alert('Failed to send email: ' + error.text);
+      });
+    } else {
+      alert('Please fill in all the fields.');
+    }
   }
 }
